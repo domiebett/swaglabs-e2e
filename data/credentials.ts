7bb34be54fcebf2a9eb0eaa8e79ad49c5cfb2ee6
Dominic Bett <dbett49@gmail.com>
2025-03-23 15:17:20 +0300

    task: env setup and login tests

---
+++ data/credentials.ts
@@ -0,0 +1,12 @@
+import { Credentials } from '../types/credentinals.types';
+
+export enum Users {
+  Standard = 'standard_user',
+}
+
+export const UserCredentials: Record<Users, Credentials> = {
+  [Users.Standard]: {
+    username: 'standard_user',
+    password: 'secret_sauce',
+  },
+};
---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+import { baseUrl } from './data/urls';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: true,
+  retries: 0,
+  use: {
+    baseURL: baseUrl,
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  outputDir: 'test-results/',
+});
